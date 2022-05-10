46e694503617601b99ef1311bfc2cfa4cb67362d
SPY-STAR0003 <spystar0003@gmail.com>
2022-05-10 16:56:44 +0430

    Refactor the Project for many mistakes

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+
+
+    return (
+        <header>
+            <h3> لیست کاربران </h3>
+            <p> برای اضافه کردن یک عضو جدید تنها کافیست روی دکمه پایین صفحه کلیک کنید </p>
+        </header>
+    )
+}
+
+export default Header;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,64 @@
 
 import React from "react";
-import AddBtn from "./js/AddBtn";
-import FirstPage from "./js/FirstPage";
 import "./style/bundle.scss"
+
+import Header from "./Header";
 function Main() {
 
     return (
-        <div>
-            <FirstPage></FirstPage>
-            <AddBtn></AddBtn>
-        </div>
+        <>
+            <Header />
+            <div className="tableBox">
+                <table>
+                    <tr>
+                        <th>نام و نام خانوادگی </th>
+                        <th> کد ملی </th>
+                        <th> پست الکترونیکی </th>
+                        <th> نوع عضویت </th>
+                        <th> دسترسی ها </th>
+                    </tr>
+                </table>
+            </div>
+            <div className={`addUsersForm d-none`}>
+                <div className="form">
+                    <span className={"closeForm"}> + </span>
+                    <h3> فرم ثبت نام اعضای جدید </h3>
+                    <div className="inputs">
+                        <div className="rightSide">
+                            <label htmlFor="familyName">نام ونام خانوادگی :</label>
+                            <input type="text" id="familyName"/>
+                            <label htmlFor="IDCode">کد ملی :</label>
+                            <input type="text" id="IDCode"/>
+                            <label htmlFor="email">پست الکترونیکی :</label>
+                            <input type="text" id="email"/>
+                        </div>
+                        <div className="leftSide">
+                            <label htmlFor="date">تاریخ عضویت :</label>
+                            <input type="text" id="date"/>
+                            <label htmlFor="education">سطح تحصیلات :</label>
+                            <input type="text" id="education"/>
+                            <label> سمت عضو جدید :</label>
+                            <div className="radio-inputs">
+                                <label className="accessRate">
+                                    ادمین
+                                    <input type="radio" name="accessRate"/>
+                                    <span className="checkmark"></span>
+                                </label>
+                                <label className="accessRate">
+                                    عضو ساده
+                                    <input type="radio" name="accessRate"/>
+                                    <span className="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <button> اضافه کردن عضو جدید </button>
+                </div>
+            </div>
+            <div className="addBtn">
+                <span> + </span>
+            </div>
+        </>
     )
 }
 

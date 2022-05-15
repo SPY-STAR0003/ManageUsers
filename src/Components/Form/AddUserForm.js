886bb804f9749aeb806a8845e982cc6b124aa39c
fatemeh-pooladrag-developer <fa.pooladrag@gmail.com>
2022-05-16 01:31:22 +0430

    Add validation to AddUserForm

--- src/Components/Form/AddUserForm.js
+++ src/Components/Form/AddUserForm.js
@@ -1,47 +1,106 @@
-import {useState} from "react";
+import { useState } from "react";
 import Input from "./Input";
 import RadioInput from "./RadioInput";
 
-function AddUserForm(props) {
+function AddUserForm({ changeUsersList, hide, formClass }) {
+    const userInitialValue = {
+        name: "",
+        IDCode: "",
+        email: "",
+        accessRate: "ادمین",
+    };
 
-    const [userState , setUserState] = useState({
-        name : "",
-        IDCode : "",
-        email : "",
-        accessRate : "",
-    })
+    const [userState, setUserState] = useState(userInitialValue)
+    const [nameError, setNameError] = useState("");
+    const [IDCodeError, setIDCodeError] = useState("");
+    const [emailError, setEmailError] = useState("");
 
-    // if we change one of state's parameters, All them change!
-    // so first we use datasets to find changed parameter!
-    // then we use prevState & update state!
-    const getInputsValue = (key, value) => setUserState({...userState, [key]: value})
+    const getInputsValue = (key, value) => setUserState({ ...userState, [key]: value })
 
+    const resetState = () => {
+        setUserState(userInitialValue)
+        setNameError("");
+        setIDCodeError("");
+        setEmailError("");
+    }
 
-    const formHandler = e => {
+    const nameValidation = (name) => {
+        if (name === "") {
+            setNameError("وارد کردن نام الزامی است")
+        } else {
+            setNameError("")
+        }
+    }
+
+    const IDCodeValidation = (IDCode) => {
+        const regex = /^\d{10}$/;
+        if (IDCode === "") {
+            setIDCodeError("وارد کردن کد ملی الزامی است")
+        } else if (!regex.test(IDCode)) {
+            setIDCodeError("کد ملی وارد شده صحیح نمی باشد")
+        } else {
+            setIDCodeError("")
+        }
+    }
+
+    const emailValidation = (email) => {
+        const regex = /^(([^<>()[\],;:\s@]+([^<>()[\],;:\s@]+)*)|(.+))@(([^<>()[\],;:\s@]+)+[^<>()[\],;:\s@]{2,})$/i;
+        if (email === "") {
+            setEmailError("وارد کردن ایمیل الزامی است")
+        } else if (!regex.test(email)) {
+            setEmailError("ایمیل وارد شده صحیح نمی باشد")
+        } else {
+            setEmailError("")
+        }
+    }
+
+    const checkValidation = () => {
+        nameValidation(userState.name);
+        IDCodeValidation(userState.IDCode);
+        emailValidation(userState.email);
+    }
+
+    const onChangeHandler = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        getInputsValue(key, value);
+        if (key === "name") {
+            nameValidation(value)
+        } else if (key === "IDCode") {
+            IDCodeValidation(value)
+        }
+        else if (key === "email") {
+            emailValidation(value)
+        }
+    }
+
+    const formHandler = (e) => {
         e.preventDefault();
-        props.changeUsersList(userState)
-        setUserState({
-            name : "",
-            IDCode : "",
-            email : "",
-            accessRate: userState.accessRate,
-        })
-        props.hide()
+        checkValidation();
+        if ((nameError === "" && IDCodeError === "" && emailError === "") &&
+            (userState.name !== "" && userState.IDCode !== "" && userState.email !== "")) {
+            changeUsersList(userState);
+            resetState();
+            hide();
+        }
     }
 
     return (
-        <div className={`addUsersForm ${props.formClass}`}>
+        <div className={`addUsersForm ${formClass}`}>
             <form className="form" onSubmit={formHandler}>
-                <span className={"closeForm"} onClick={props.hide}> + </span>
+                <span className={"closeForm"} onClick={() => { resetState(); hide() }}> + </span>
                 <h3> فرم ثبت نام اعضای جدید </h3>
                 <div className="inputs">
                     <div className="rightSide">
-                        <Input label={"نام و نام خانوادگی :"} type={"text"} value={userState.name}  name={"name"} onchangeFunction={(e) => getInputsValue('name', e.target.value)} />
-                        <Input label={"کد ملی :"} type={"text"} value={userState.IDCode}  name={"IDCode"} onchangeFunction={(e) => getInputsValue('IDCode', e.target.value)} />
+                        <Input label={"نام و نام خانوادگی :"} type="text" value={userState.name} name={"name"} onchangeFunction={onChangeHandler} />
+                        {nameError && <p className="error">{nameError}</p>}
+                        <Input label={"کد ملی :"} type="text" value={userState.IDCode} name={"IDCode"} onchangeFunction={onChangeHandler} />
+                        {IDCodeError && <p className="error">{IDCodeError}</p>}
                     </div>
                     <div className="leftSide">
-                        <Input label={"ایمیل :"} type={"text"} value={userState.email}  name={"email"} onchangeFunction={(e) => getInputsValue('email', e.target.value)} />
-                        <RadioInput mainLabel={"میزان دسترسی کاربر :"} label1={"ادمین"} label2={"عضوساده"} name={"accessRate"} onchangeFunction={getInputsValue}/>
+                        <Input label={"ایمیل :"} type="text" value={userState.email} name={"email"} onchangeFunction={onChangeHandler} />
+                        {emailError && <p className="error">{emailError}</p>}
+                        <RadioInput mainLabel={"میزان دسترسی کاربر :"} label1={"ادمین"} label2={"عضوساده"} name={"accessRate"} onchangeFunction={getInputsValue} selected={userState.accessRate} />
                     </div>
                 </div>
                 <button type="submit"> اضافه کردن عضو جدید </button>
@@ -49,5 +108,4 @@ function AddUserForm(props) {
         </div>
     )
 }
-
 export default AddUserForm;
--- src/Components/Form/RadioInput.js
+++ src/Components/Form/RadioInput.js
@@ -1,16 +1,16 @@
-export default function RadioInput({ mainLabel , label1 , label2 , name , onchangeFunction}) {
+export default function RadioInput({ mainLabel, label1, label2, name, onchangeFunction, selected }) {
     return (
         <>
             <label> {mainLabel} </label>
             <div className="radio-inputs">
                 <label className="accessRate">
                     {label1}
-                    <input type="radio" name={name} onChange={(e) => onchangeFunction("accessRate" , e.target.dataset.rate)} data-name={`${name}-1`} data-rate={label1}/>
+                    <input type="radio" name={name} onChange={(e) => onchangeFunction("accessRate", e.target.dataset.rate)} data-name={`${name}-1`} data-rate={label1} checked={selected === label1} />
                     <span className="checkmark"></span>
                 </label>
                 <label className={name}>
                     {label2}
-                    <input type="radio" name={name} onChange={(e) => onchangeFunction("accessRate" , e.target.dataset.rate)} data-name={`${name}-2`} data-rate={label2}/>
+                    <input type="radio" name={name} onChange={(e) => onchangeFunction("accessRate", e.target.dataset.rate)} data-name={`${name}-2`} data-rate={label2} checked={selected === label2} />
                     <span className="checkmark"></span>
                 </label>
             </div>
